19d4f8f9d776d7b3b8d19b6ca0c5809e707c07d0
mahibulmasud <mahibulmasud@gmail.com>
2021-02-02 17:01:43 +0600

    letconst

---
+++ letconst.js
@@ -0,0 +1,16 @@
+// const age = 12;
+// age = 15;
+// console.log(age);
+
+const numbers = [12,45];
+numbers[1] = 88;
+numbers.push(12);
+// numbers = ['a','b','c'];
+console.log(numbers);
+// numbers[1]
+
+let patientName = 'Rahim';
+patientName = 'Rahima';
+console.log(patientName);
+
+//we can not access data from outside  of loop
