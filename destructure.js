9d79b1db88a5d2e43de717d63de2a65fa7c3f6c9
mahibulmasud <mahibulmasud@gmail.com>
2021-02-02 23:49:47 +0600

    k

---
+++ destructure.js
@@ -0,0 +1,19 @@
+const person = {
+    name:'Jack William', 
+    age: 17,
+    job: 'facebooker', 
+    gfName:'Ema Watson', 
+    address: 'kochu khet', 
+    phone: '01980573601',
+    friends:['kali','sali','mali']
+}
+const {phone,gfName,friends,salay}= person;
+// const {phone} =person;
+// const {gfName} = person;
+// const gfName = person.gfName;
+// const phone = person.phone;
+
+console.log(gfName,phone,friends);
+console.log(gfName,phone,salay);
+console.log(gfName,phone);
+console.log(gfName,phone);
